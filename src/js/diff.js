8249685f4c190c53fee03ac8272371801c001866
eczn <1975845422@qq.com>
2018-01-27 16:15:56 +0800

    [A] diff

--- src/js/diff.js
+++ src/js/diff.js
@@ -1,99 +1,68 @@
 // diff.js
 const VNode = require('./VNode')
+    , list_diff = require('list-diff2')
 
-/**
- * @description 在 children 里查找字符串节点
- * @param { String } str 
- * @param { Array<VNode> } children 
- * @returns { Number } position 下标 
- */
-function findString(str, children){
-    let pos = null; 
-    children.forEach((child, idx) => {
-        if (typeof child === 'string' && child === str){
-            pos = idx; 
-        }
-    }); 
+const REMOVE = 0
+    , INSERT = 1
+    , TYPES = { REMOVE, INSERT }
 
-    return pos; 
-}
-
-/**
- * @description 在 children 里查找 vid 节点 
- * @param { String | Number } vid 
- * @param { Array<VNode> } children 
- * @returns { Number } position 下标 
- */
-function findVid(target_vid, children){
-    return children.reduce((acc, child, idx) => {
-        if (typeof child === 'string'){
-            return acc; 
-        } else {
-            let { vid } = child; 
-
-            return target_vid === vid ?
-                idx : acc; 
-        }
-    }, null); 
-}
-
+// Set To Diff
+diff.TYPES = TYPES; 
 
 /**
  * @description diff t1 and t2 
  * @param { VNode } t1 源树 
  * @param { VNode } t2 目标树 
+ * @returns { Object } diff 结果 
  */
 function diff(t1, t2){
     let strTable = {}
-    let allDiff = {}
 
-    // 不存在任意一个就 return 
-    if (!t1 || !t2) return {}; 
+    if (!t1 || !t2) return null; 
     
-    // t2ChildrenMap
-    allDiff.children = t2.children.reduce((acc, t2_child, idx) => {
-        let l_idx = null; 
+    let diffRes =  list_diff(t1.children, t2.children, 'vid');
 
-        if (typeof t2_child === 'string'){
-            // t2_child, t1.children
 
-            // 遍历 t1.children 
-            for (let idx = 0; idx < t1.children.length; idx++) {
-                let child = t1.children[idx]; 
-
-                if (typeof child === 'string' && child === t2_child){
-                    if (!strTable[idx]){
-                        l_idx = idx; 
-                        strTable[idx] = true; 
-                        break; 
-                    }
-                }            
-            }
-
-        } else {
-            l_idx = findVid(t2_child.vid, t1.children); 
-        }
-
-        acc[idx] = l_idx; 
-        return acc; 
-    }, []); 
-
-    // 标签名 diff 
-    allDiff.tag = [t1.tag, t2.tag]; 
-
-    // 属性集 diff 
     let propDiff = {}; 
     Object.keys(t1.props).forEach(t1_prop => {
         let t1_val = t1.props[t1_prop]; 
         let t2_val = t2.props[t1_prop]; 
         if (t1_val != t2_val) {
-            propDiff[t1_prop] = [t1_val, t2_val]; 
+            propDiff[t1_prop] = t2_val; 
         }
     }); 
 
-    allDiff.props = propDiff; 
+    diffRes.props = propDiff
 
-    return allDiff; 
+    return diffRes; 
+}
+
+/**
+ * @description 把 diff 结果应用到 t1 上
+ * @param { VNode } t1 
+ * @param { * } diffRes 
+ */
+diff.patch = (t1, diffRes) => {
+    if (!diffRes) return t1; 
+
+    let { moves, props } = diffRes; 
+    let oldList = t1.children; 
+
+    moves.forEach(move => {
+        if (move.type === REMOVE) {
+            oldList.splice(move.index, 1);
+        } else {
+            oldList.splice(move.index, 0, move.item);
+        }
+    }); 
+
+    Object.keys(props).forEach(key => {
+        let newVal = props[key]; 
+
+        t1.props[key] = newVal; 
+    }); 
+
+    return t1; 
 }
 
 module.exports = diff; 
