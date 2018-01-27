b3e76cc97e0f199214a9fa735afc7a8c49557308
eczn <1975845422@qq.com>
2018-01-27 15:20:31 +0800

    [A] 添加文本节点的获取

--- src/js/from$Node.js
+++ src/js/from$Node.js
@@ -1,31 +1,37 @@
-const { h } = require('./VNode'); 
+const VNode = require('./VNode'); 
+
+let { h } = VNode; 
 
 /**
  * @description 根据 DOM 节点构造 VNode 对象
- * @param {Node} 节点 DOM 
+ * @param   { Node  } 节点 DOM 
+ * @returns { VNode } 虚拟 DOM 
  */
 function from$Node($node){
-    let tag = $node.tagName.toLowerCase(); 
+    let { nodeType } = $node; 
+    if (nodeType === 3){
+        return new VNode.Text($node.nodeValue); 
+    } else if (nodeType === 1) {
+        let tag = $node.tagName.toLowerCase(); 
+        
+        let props = Array.from($node.attributes).reduce((props, attr) => {
+            let { name, value } = attr; 
     
-    let props = Array.from($node.attributes).reduce((props, attr) => {
-        let { name, value } = attr; 
-
-        props[name] = value; 
-
-        return props; 
-    }, {}); 
-
-    let $children = Array.from($node.childNodes); 
-
-    let children = $children.map($child => {
-        if ($child.nodeType === 3){
-            return $child.nodeValue; 
-        } else if ($child.nodeType === 1) {
-            return from$Node($child); 
-        }
-    }).filter(e => e); 
-
-    return h(tag, props, children); 
+            props[name] = value; 
+    
+            return props; 
+        }, {}); 
+    
+        let $children = Array.from($node.childNodes); 
+    
+        let children = $children.map($child => {
+            if ($child.nodeType === 3 || $child.nodeType === 1){
+                return from$Node($child); 
+            }
+        }).filter(e => e); 
+    
+        return h(tag, props, children);         
+    }
 }
 
 module.exports = from$Node; 
