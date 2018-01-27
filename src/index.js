6ccad45d2b893205b1ebd7e321c7b11212f9f299
eczn <1975845422@qq.com>
2018-01-27 16:16:31 +0800

    [A] 测试数据

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
 const { h } = require('./js/VNode')
     , from$Node = require('./js/from$Node')
     , diff = require('./js/diff')
+    , patch = require('./js/patch')
 
 
 const $app = document.getElementById('app-1'); 
 const $app2 = document.getElementById('app-2'); 
-const target = document.getElementById('target'); 
+const $target = document.getElementById('target'); 
+const btn = document.getElementById('to-patch'); 
 
 let t1 = from$Node($app); 
-
-
 let t2 = from$Node($app2); 
+let diffRes = diff(t1, t2);
+
 window.$app = $app; 
 window.t1 = t1;
 window.t2 = t2; 
-window.diff = diff
+window.diffRes = diffRes; 
+
+$app.remove();
+$app2.remove(); 
 
 
 
+t1.$mount($target); 
+
+console.time('VDOM'); 
+t1.$patch(diffRes); 
+t1.patch(diffRes); 
+console.timeEnd('VDOM'); 
+
+
+// diff.patch(t1, diffRes); 
