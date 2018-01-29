3241ee396d1f5be1a8301c2e704340ead3913b38
bozhi.lai <bozhi.lai@vipshop.com>
2018-01-29 15:23:26 +0800

    [A] readme

--- README.md
+++ README.md
@@ -1,18 +1,37 @@
-# vdom 
+# VDOM 
 
-利用了 list-diff 去实现虚拟 DOM
+1. 实现虚拟 DOM
+2. 利用这个虚拟 DOM 实现一个简单的前端框架 Mois (这次仿 React)
 
-# build 
+# Build 
 
 ``` bash 
 $ npm i 
 $ gulp serve 
 ```
 
-# usage 
+# Usage 
 
-请看 src/index.html 和 src/index.js 
+``` html
+<div id="app"></div>
+```
 
+``` js
+const VDOM = require('./js/VirualDOM')
+    , { h, from$Node, from$Html } = VDOM
+
+// 生成 VDOM 
+let $app = from$Html(`
+    <div class="app">
+        <p>hello</p>
+    </div>
+`); 
+
+// 虚拟 DOM 挂到 app 上
+$app.$mount(
+    document.getElementById('app')
+); 
+```
 
 # License
 
--- src/index.js
+++ src/index.js
@@ -1,56 +1,13 @@
-const { h } = require('./js/VNode')
-    , from$Node = require('./js/from$Node')
-    , diff = require('./js/diff')
+const VDOM = require('./js/VirualDOM')
+    , { h, from$Node, from$Html } = VDOM
 
+let $app = from$Html(`
+    <div class="app">
+        <p>hello</p>
+    </div>
+`); 
 
-const $app = document.getElementById('app-1'); 
-const $app2 = document.getElementById('app-2'); 
-const $target = document.getElementById('target'); 
-const $native_target = document.getElementById('native-target'); 
-const btn = document.getElementById('to-patch'); 
-
-let t1 = from$Node($app); 
-let t2 = from$Node($app2); 
-
-
-
-
-// diff.patch(t1, diffRes); 
-
-window.$app = $app;
-window.$app2 = $app2;  
-window.t1 = t1;
-window.t2 = t2; 
- 
-
-
-// $app2.remove(); 
-
-console.time('diff');
-let diffRes = diff(t1, t2);
-console.timeEnd('diff');
-window.diffRes = diffRes;
-
-console.time('$mount');
-    t1.$mount($target); 
-console.timeEnd('$mount');
-
-
-console.time('$patch');
-t1.$patch(diffRes); 
-console.timeEnd('$patch');
-
-console.time('patch');
-t1.patch(diffRes); 
-console.timeEnd('patch');
-
-
-console.time('native'); 
-let html1 = $app.outerHTML; 
-let html2 = $app2.outerHTML; 
-$app2.outerHTML = html2; 
-console.timeEnd('native'); 
-
-// diff.patch(t1, diffRes); 
-
-$app.remove();
+// 虚拟 DOM 挂到 app 上
+$app.$mount(
+    document.getElementById('app')
+); 
