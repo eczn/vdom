d53f3faa8f82d8d2462dc3816f152c5858934606
bozhi.lai <bozhi.lai@vipshop.com>
2018-01-29 14:32:28 +0800

    [M] 测试

--- src/index.js
+++ src/index.js
@@ -6,28 +6,51 @@ const { h } = require('./js/VNode')
 const $app = document.getElementById('app-1'); 
 const $app2 = document.getElementById('app-2'); 
 const $target = document.getElementById('target'); 
+const $native_target = document.getElementById('native-target'); 
 const btn = document.getElementById('to-patch'); 
 
 let t1 = from$Node($app); 
 let t2 = from$Node($app2); 
-let diffRes = diff(t1, t2);
-
-window.$app = $app; 
-window.t1 = t1;
-window.t2 = t2; 
-window.diffRes = diffRes; 
-
-$app.remove();
-$app2.remove(); 
 
 
-
-t1.$mount($target); 
-
-console.time('VDOM'); 
-t1.$patch(diffRes); 
-// t1.patch(diffRes); 
-console.timeEnd('VDOM'); 
 
 
 // diff.patch(t1, diffRes); 
+
+window.$app = $app;
+window.$app2 = $app2;  
+window.t1 = t1;
+window.t2 = t2; 
+ 
+
+
+// $app2.remove(); 
+
+console.time('diff');
+let diffRes = diff(t1, t2);
+console.timeEnd('diff');
+window.diffRes = diffRes;
+
+console.time('$mount');
+    t1.$mount($target); 
+console.timeEnd('$mount');
+
+
+console.time('$patch');
+t1.$patch(diffRes); 
+console.timeEnd('$patch');
+
+console.time('patch');
+t1.patch(diffRes); 
+console.timeEnd('patch');
+
+
+console.time('native'); 
+let html1 = $app.outerHTML; 
+let html2 = $app2.outerHTML; 
+$app2.outerHTML = html2; 
+console.timeEnd('native'); 
+
+// diff.patch(t1, diffRes); 
+
+$app.remove();
--- src/js/test.js
+++ src/js/test.js
@@ -1,9 +1,31 @@
 let diff = require('./list-diff'); 
 
-let list_a = [1, 2,  3, 4, 5, 6, 7, 8]; 
-let list_b = [3, 10, 4, 5, 9, 8, 7]; 
+let list_a = [1, 2,  3, 4, 5, 6, 7, 8, 9, 100, 123123]
+let list_b = [3, 10, 4, 55, 9, 99, 7]
 
 let patches = diff(list_a, list_b)
 
-console.log(patches)
+let { INSERT, REORDER, DELETE } = diff; 
 
+
+
+// patches.forEach(patch => {
+//     let { type, idx, item } = patch; 
+
+//     if (type === DELETE){
+//         list_a.splice(idx, 1); 
+//     } else if (type === INSERT){
+//         list_a.splice(idx, 0, item); 
+//     } else if (type === REORDER){
+//         let exIdx = item; 
+//         let a = list_a[idx]; 
+        
+//         list_a[idx]   = list_a[exIdx]; 
+//         list_a[exIdx] = a; 
+//     }
+// })
+
+console.log(patches)
+// patches.to(list_a); 
+
+// console.log(list_a)
